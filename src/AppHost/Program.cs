94dc7b666a377e5dfea39a30d02b557e10835e55
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-04-30 15:40:53 +0100

    Add Aspire

---
+++ src/AppHost/Program.cs
@@ -0,0 +1,10 @@
+﻿using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+var backend = builder.AddProject<Backend>("backend");
+
+builder.AddProject<StaffWebUI>("staffwebui")
+    .WithReference(backend);
+
+builder.Build().Run();
