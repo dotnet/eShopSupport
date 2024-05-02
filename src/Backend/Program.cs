dd25782e734eb48f9df299105a6e0d0fa8683c29
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-05-02 17:33:23 +0100

    Fetch real virtualized data

--- src/Backend/Program.cs
+++ src/Backend/Program.cs
@@ -1,4 +1,5 @@
-﻿using eShopSupport.Backend.Data;
+﻿using System.ComponentModel.DataAnnotations;
+using eShopSupport.Backend.Data;
 using eShopSupport.ServiceDefaults.Clients.Backend;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,7 +15,44 @@ await AppDbContext.EnsureDbCreatedAsync(app.Services);
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapGet("/tickets", (AppDbContext dbContext) => dbContext.Tickets.Select(t =>
-    new ListTicketsResult(t.TicketId, t.CustomerFullName, t.Messages.Count)).ToListAsync());
+app.MapGet("/tickets", async (AppDbContext dbContext, int startIndex, int maxResults, string? sortBy, bool sortAscending) =>
+{
+    if (maxResults > 100)
+    {
+        return Results.BadRequest("maxResults must be 100 or less");
+    }
+
+    IQueryable<Ticket> itemsMatchingFilter = dbContext.Tickets;
+
+    if (!string.IsNullOrEmpty(sortBy))
+    {
+        switch (sortBy)
+        {
+            case "TicketId":
+                itemsMatchingFilter = sortAscending == true
+                    ? itemsMatchingFilter.OrderBy(t => t.TicketId)
+                    : itemsMatchingFilter.OrderByDescending(t => t.TicketId);
+                break;
+            case "CustomerFullName":
+                itemsMatchingFilter = sortAscending == true
+                    ? itemsMatchingFilter.OrderBy(t => t.CustomerFullName)
+                    : itemsMatchingFilter.OrderByDescending(t => t.CustomerFullName);
+                break;
+            case "NumMessages":
+                itemsMatchingFilter = sortAscending == true
+                    ? itemsMatchingFilter.OrderBy(t => t.Messages.Count)
+                    : itemsMatchingFilter.OrderByDescending(t => t.Messages.Count);
+                break;
+            default:
+                return Results.BadRequest("Invalid sortBy value");
+        }
+    }
+
+    var resultItems = itemsMatchingFilter
+        .Skip(startIndex)
+        .Take(maxResults)
+        .Select(t => new ListTicketsResultItem(t.TicketId, t.CustomerFullName, t.Messages.Count));
+    return Results.Ok(new ListTicketsResult(await resultItems.ToListAsync(), await itemsMatchingFilter.CountAsync()));
+});
 
 app.Run();
--- src/ServiceDefaults/Clients/Backend/BackendClient.cs
+++ src/ServiceDefaults/Clients/Backend/BackendClient.cs
@@ -1,12 +1,15 @@
-﻿using System.Net.Http.Json;
+﻿using System.Collections;
+using System.Net.Http.Json;
 
 namespace eShopSupport.ServiceDefaults.Clients.Backend;
 
 public class BackendClient(HttpClient http)
 {
-    public Task<ListTicketsResult[]> ListTicketsAsync()
-        => http.GetFromJsonAsync<ListTicketsResult[]>("/tickets")!;
+    public Task<ListTicketsResult> ListTicketsAsync(int startIndex, int maxResults, string? sortBy, bool? sortAscending)
+        => http.GetFromJsonAsync<ListTicketsResult>($"/tickets?startIndex={startIndex}&maxResults={maxResults}&sortBy={sortBy}&sortAscending={sortAscending}")!;
 }
 
-public record ListTicketsResult(
+public record ListTicketsResult(ICollection<ListTicketsResultItem> Items, int TotalCount);
+
+public record ListTicketsResultItem(
     int TicketId, string CustomerFullName, int NumMessages);
