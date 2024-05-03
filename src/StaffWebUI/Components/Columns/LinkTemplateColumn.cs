3ea5cc50a5c94990284c7c23af1ef940748a9867
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-05-03 12:43:37 +0100

    Use proper links and clean up code

---
+++ src/StaffWebUI/Components/Columns/LinkTemplateColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.QuickGrid;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace eShopSupport.StaffWebUI.Components
+{
+    public class LinkTemplateColumn<TGridItem> : TemplateColumn<TGridItem>
+    {
+        [Parameter]
+        public required Func<TGridItem, string>? Href { get; set; }
+
+        protected override void CellContent(RenderTreeBuilder builder, TGridItem item)
+        {
+            builder.OpenElement(0, "a");
+            builder.AddAttribute(1, "class", "link-col");
+            builder.AddAttribute(2, "href", Href?.Invoke(item));
+            builder.OpenRegion(3);
+            base.CellContent(builder, item);
+            builder.CloseRegion();
+            builder.CloseElement();
+        }
+    }
+}
