99a8f75bc0154ed9d4fc434312563ee7b9139c93
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-05-07 10:48:37 +0100

    Make theme switching work correctly

--- src/StaffWebUI/Components/Layout/ThemePicker.razor.js
+++
@@ -1,3 +0,0 @@
-﻿export function systemThemeIsDark() {
-    return !!(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches);
-}
---
+++ src/StaffWebUI/wwwroot/theme-info.js
@@ -0,0 +1,29 @@
+﻿// This needs to be a custom element, because we need it to run during prerendering and immediately
+// after FluentUI's <loading-theme> custom element initializes. Otherwise there's a flash of wrong styling.
+// This also means it has to be a regular script file and not a module loaded via JS interop.
+
+class LoadingThemeInfo extends HTMLElement {
+    static getEffectiveTheme() {
+        const explicitTheme = document.querySelector('fluent-design-theme').getAttribute('mode');
+        switch (explicitTheme) {
+            case 'dark':
+            case 'light':
+                return explicitTheme;
+            default:
+                // It's using system, or no theme is set so it defaults to system
+                return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+        }
+    }
+
+    static updateEffectiveThemeClass() {
+        document.documentElement.classList.toggle('dark-mode', LoadingThemeInfo.getEffectiveTheme() === 'dark');
+
+    }
+
+    connectedCallback() {
+        LoadingThemeInfo.updateEffectiveThemeClass();
+    }
+}
+
+window.customElements.define('loading-theme-info', LoadingThemeInfo);
+window.LoadingThemeInfo = LoadingThemeInfo;
