a88269c0fb9180924d99894ad3c172d7688a284a
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-05-10 12:51:35 +0100

    Handle errors during chat completion

--- src/Backend/Api/Assistant.cs
+++ src/Backend/Api/Assistant.cs
@@ -8,16 +8,27 @@ public static class Assistant
 {
     public static void MapAssistantEndpoints(this WebApplication app)
     {
-        app.MapPost("/api/assistant/chat", async (HttpContext httpContext, IChatCompletionService chatService, AssistantChatRequest chatRequest) =>
+        app.MapPost("/api/assistant/chat", async (HttpContext httpContext, IChatCompletionService chatService, ILoggerFactory loggerFactory, AssistantChatRequest chatRequest) =>
         {
             var chatHistory = new ChatHistory(chatRequest.Messages.Select(m => new ChatMessageContent(
                 m.IsAssistant ? AuthorRole.Assistant : AuthorRole.User,
                 m.Text)));
 
-            var streamingResponse = chatService.GetStreamingChatMessageContentsAsync(chatHistory);
-            await foreach (var chunk in streamingResponse)
+            try
             {
-                await httpContext.Response.WriteAsync(chunk.ToString());
+                var streamingResponse = chatService.GetStreamingChatMessageContentsAsync(chatHistory);
+
+                await foreach (var chunk in streamingResponse)
+                {
+                    await httpContext.Response.WriteAsync(chunk.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                // We don't want to return the raw exception to the response as it would then show up in the chat UI
+                var logger = loggerFactory.CreateLogger(typeof(Assistant).FullName!);
+                logger.LogError(ex, "Error during chat completion.");
+                await httpContext.Response.WriteAsync("Sorry, there was a problem. Please try again.");
             }
         });
     }
