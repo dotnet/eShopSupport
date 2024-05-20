a121fba2f94c6fb63d61bfd87d5cb5c2a4ad094c
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-05-20 14:24:22 +0100

    Make a custom element to display the message text

---
+++ src/StaffWebUI/Components/Pages/Ticket/TicketAssistantMessage.razor.js
@@ -0,0 +1,14 @@
+﻿window.customElements.define('assistant-message', class extends HTMLElement {
+    static observedAttributes = ['message'];
+
+    connectedCallback() {
+        this.textContent = this.getAttribute('message') || '...';
+    }
+
+    // Whenever the message attribute changes, update the text content of the element
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === 'message') {
+            this.textContent = newValue;
+        }
+    }
+});
