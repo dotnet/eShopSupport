381c88f158091cac8796414466aa92b7fb5a7739
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-05-21 17:25:57 +0100

    Start inserting actual context

---
+++ src/StaffWebUI/Components/Pages/Ticket/Ticket.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace eShopSupport.StaffWebUI.Components.Pages.Ticket;
+
+public partial class Ticket
+{
+    private string GetSmartTextAreaRole()
+    {
+        if (ticket is null)
+        {
+            return smartTextAreaRolePrefix;
+        }
+        else
+        {
+            return $$"""
+            {{smartTextAreaRolePrefix}}, currently helping a customer {{ticket.CustomerFullName}} with this enquiry:
+            <ticket_summary>{{ticket.LongSummary}}</ticket_summary>
+            <first_customer_message>{{ticket.Messages.FirstOrDefault(m => m.AuthorName != "Support")?.MessageText}}</first_customer_message>
+            <last_customer_message>{{ticket.Messages.LastOrDefault(m => m.AuthorName != "Support")?.MessageText}}</first_customer_message>
+            """;
+        }
+    }
+}
