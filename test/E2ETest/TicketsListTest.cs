dd2e202a8ca03f13b2a707a7f497c6ae30a3264d
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-05-30 13:11:31 +0100

    Add Playwright

---
+++ test/E2ETest/TicketsListTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+using E2ETest.Infrastructure;
+using Microsoft.Playwright;
+using static Microsoft.Playwright.Assertions;
+namespace E2ETest;
+
+[Collection(AppTestCollection.Name)]
+public class TicketsListTest(AppHostFixture app) : IAsyncLifetime
+{
+    protected IPlaywright Playwright { get; private set; } = default!;
+    protected IBrowser Browser { get; private set; } = default!;
+    protected IPage Page { get; private set; } = default!;
+
+    public async Task InitializeAsync()
+    {
+        Playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+        Browser = await Playwright.Chromium.LaunchAsync(new()
+        {
+            Headless = !Debugger.IsAttached,
+        });
+
+        if (Page is not null)
+        {
+            // Otherwise we have to deal with unsubscribing from Page.PageError
+            throw new InvalidOperationException("Cannot intialize a new page when one is already initialized");
+        }
+
+        Page = await Browser.NewPageAsync();
+        Page.PageError += (_, message)
+            => throw new InvalidOperationException("Page error: " + message);
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Browser.DisposeAsync();
+        Playwright.Dispose();
+    }
+
+    [Fact]
+    public async Task HasPageTitle()
+    {
+        var httpClient = await app.StaffWebUI.CreateHttpClientAsync();
+        await Page.GotoAsync(httpClient.BaseAddress!.ToString());
+        await Expect(Page).ToHaveTitleAsync("eShopSupport: Tickets");
+    }
+}
