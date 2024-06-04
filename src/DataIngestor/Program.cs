a170c657edbd5a766b307bbe89be7ffeb58ed9d6
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-06-04 11:12:33 +0100

    Add demo seed data

--- src/DataIngestor/Program.cs
+++ src/DataIngestor/Program.cs
@@ -34,7 +34,7 @@ if (!Directory.Exists(generatedDataPath))
 }
 
 var solutionDir = PathUtils.FindAncestorDirectoryContaining("*.sln");
-var outputDir = Path.Combine(solutionDir, "seeddata", "dev");
+var outputDir = Path.Combine(solutionDir, "seeddata", "demo");
 Directory.CreateDirectory(outputDir);
 
 await new TicketIngestor().RunAsync(generatedDataPath, outputDir);
