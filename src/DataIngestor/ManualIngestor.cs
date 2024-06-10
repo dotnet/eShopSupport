0e55748d7a926b066f35bb768e5219ad3dae22e4
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-06-10 09:21:15 +0100

    Clean up PDFPig usage

--- src/DataIngestor/ManualIngestor.cs
+++ src/DataIngestor/ManualIngestor.cs
@@ -1,6 +1,5 @@
 ﻿using System.Runtime.InteropServices;
 using System.Text.Json;
-using AspirePython.VectorDbIngestor;
 using eShopSupport.Backend.Data;
 using Microsoft.SemanticKernel.Text;
 using SmartComponents.LocalEmbeddings.SemanticKernel;
@@ -8,7 +7,6 @@ using UglyToad.PdfPig;
 using UglyToad.PdfPig.Content;
 using UglyToad.PdfPig.DocumentLayoutAnalysis.PageSegmenter;
 using UglyToad.PdfPig.DocumentLayoutAnalysis.WordExtractor;
-using UglyToad.PdfPig.Util;
 
 public class ManualIngestor
 {
@@ -16,48 +14,54 @@ public class ManualIngestor
     {
         Console.WriteLine("Ingesting manuals...");
 
-        // Chunk and embed them
+        // Prepare
         var manualsSourceDir = Path.Combine(sourceDir, "manuals", "pdf");
-        //using var tika = new Tika();
         using var embeddingGenerator = new LocalTextEmbeddingGenerationService();
         var chunks = new List<ManualChunk>();
         var paragraphIndex = 0;
+
+        // Loop over each PDF file
         foreach (var file in Directory.GetFiles(manualsSourceDir, "*.pdf"))
         {
             Console.WriteLine($"Generating chunks for {file}...");
             var docId = int.Parse(Path.GetFileNameWithoutExtension(file));
-            using PdfDocument document = PdfDocument.Open(file);
-            foreach (var page in document.GetPages())
-            {
-                var letters = page.Letters;
-                var wordExtractor = NearestNeighbourWordExtractor.Instance;
-                var words = wordExtractor.GetWords(letters);
-                var pageSegmenter = DocstrumBoundingBoxes.Instance;
-                var textBlocks = pageSegmenter.GetBlocks(words);
-                var pageText = string.Join(Environment.NewLine + Environment.NewLine, textBlocks.Select(t => t.Text.ReplaceLineEndings(" ")));
-                
 
+            // Loop over each page in it
+            using var pdf = PdfDocument.Open(file);
+            foreach (var page in pdf.GetPages())
+            {
+                // [1] Parse (PDF page -> string)
+                var pageText = GetPageText(page);
+
+                // [2] Chunk (split into shorter strings on natural boundaries)
                 var paragraphs = TextChunker.SplitPlainTextParagraphs([pageText], 200);
+
+                // [3] Embed (map into semantic space)
                 var paragraphsWithEmbeddings = paragraphs.Zip(await embeddingGenerator.GenerateEmbeddingsAsync(paragraphs));
 
-                foreach (var p in paragraphsWithEmbeddings)
+                // [4] Save
+                chunks.AddRange(paragraphsWithEmbeddings.Select(p => new ManualChunk
                 {
-                    var chunk = new ManualChunk
-                    {
-                        ProductId = docId,
-                        PageNumber = page.Number,
-                        ChunkId = ++paragraphIndex,
-                        Text = p.First,
-                        Embedding = MemoryMarshal.AsBytes(p.Second.Span).ToArray()
-                    };
-                    chunks.Add(chunk);
-                }
+                    ProductId = docId,
+                    PageNumber = page.Number,
+                    ChunkId = ++paragraphIndex,
+                    Text = p.First,
+                    Embedding = MemoryMarshal.AsBytes(p.Second.Span).ToArray()
+                }));
             }
-            
         }
 
         var outputOptions = new JsonSerializerOptions { WriteIndented = true };
         await File.WriteAllTextAsync(Path.Combine(outputDir, "manual-chunks.json"), JsonSerializer.Serialize(chunks, outputOptions));
         Console.WriteLine($"Wrote {chunks.Count} manual chunks");
+    }
+
+    private static string GetPageText(Page pdfPage)
+    {
+        var letters = pdfPage.Letters;
+        var words = NearestNeighbourWordExtractor.Instance.GetWords(letters);
+        var textBlocks = DocstrumBoundingBoxes.Instance.GetBlocks(words);
+        return string.Join(Environment.NewLine + Environment.NewLine,
+            textBlocks.Select(t => t.Text.ReplaceLineEndings(" ")));
     }
 }
