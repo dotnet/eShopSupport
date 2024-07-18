3b119921b049b04faca7ee0718f24e2f5bd873c8
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-07-18 17:52:24 +0100

    Use native SK function calling, which currently will only work with OpenAI

--- src/Backend/Api/AssistantApi.cs
+++ src/Backend/Api/AssistantApi.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.ComponentModel;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
@@ -34,7 +34,7 @@ public static class AssistantApi
         chatHistory.AddSystemMessage($$"""
             You are a helpful AI assistant called 'Assistant' whose job is to help customer service agents working for AdventureWorks, an online retailer.
             The customer service agent is currently handling the following ticket:
-                
+
             <product_id>{{request.ProductId}}</product_id>
             <product_name>{{product?.Model ?? "None specified"}}</product_name>
             <customer_name>{{request.CustomerName}}</customer_name>
@@ -43,28 +43,23 @@ public static class AssistantApi
             The most recent message from the customer is this:
             <customer_message>{{request.TicketLastCustomerMessage}}</customer_message>
             However, that is only provided for context. You are not answering that question directly. The real question will be asked by the user below.
-            """);
 
-        chatHistory.AddRange(request.Messages.Select(m => new ChatMessageContent(m.IsAssistant ? AuthorRole.Assistant : AuthorRole.User, m.Text)));
+            If this is a question about the product, ALWAYS search the product manual.
 
-        var toolOutputs = await RunRetrievalLoopUntilReadyToAnswer(httpContext.Response, chatService, manualSearch, new ChatHistory(chatHistory), cancellationToken);
-        chatHistory.AddRange(toolOutputs);
-
-        chatHistory.AddSystemMessage("Based on this context, provide an answer to the user's question.");
-
-        if (toolOutputs.Any())
-        {
-            chatHistory.AddSystemMessage($$"""
-            ALWAYS justify your answer by citing the most relevant one of the above search results. Do this by including this syntax in your reply:
+            ALWAYS justify your answer by citing a search result. Do this by including this syntax in your reply:
             <cite searchResultId=number>shortVerbatimQuote</cite>
             shortVerbatimQuote must be a very short, EXACT quote (max 10 words) from whichever search result you are citing.
             Only give one citation per answer. Always give a citation because this is important to the business.
             """);
-        }
 
-        var executionSettings = new OpenAIPromptExecutionSettings { ResponseFormat = "text", Seed = 0, Temperature = 0 };
+        chatHistory.AddRange(request.Messages.Select(m => new ChatMessageContent(m.IsAssistant ? AuthorRole.Assistant : AuthorRole.User, m.Text)));
+
+        var kernel = new Kernel();
+        kernel.ImportPluginFromObject(new SearchManualPlugin(httpContext, manualSearch));
+
+        var executionSettings = new OpenAIPromptExecutionSettings { Seed = 0, Temperature = 0, ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
         var answerBuilder = new StringBuilder();
-        await foreach (var chunk in chatService.GetStreamingChatMessageContentsAsync(chatHistory, executionSettings, cancellationToken: cancellationToken))
+        await foreach (var chunk in chatService.GetStreamingChatMessageContentsAsync(chatHistory, executionSettings, kernel, cancellationToken: cancellationToken))
         {
             await httpContext.Response.WriteAsync(",\n");
             await httpContext.Response.WriteAsync(JsonSerializer.Serialize(new AssistantChatReplyItem(AssistantChatReplyItemType.AnswerChunk, chunk.ToString())));
@@ -98,66 +93,36 @@ public static class AssistantApi
         public bool IsAddressedByNameToCustomer { get; set; }
     }
 
-    private static async Task<IReadOnlyList<ChatMessageContent>> RunRetrievalLoopUntilReadyToAnswer(HttpResponse httpResponse, IChatCompletionService chatService, ProductManualSemanticSearch manualSearch, ChatHistory chatHistory, CancellationToken cancellationToken)
+    private class SearchManualPlugin(HttpContext httpContext, ProductManualSemanticSearch manualSearch)
     {
-        chatHistory.AddSystemMessage($$"""
-            Your goal is to decide how the agent's FINAL question can best be processed. Do not reply to the agent's question directly,
-            but instead return a JSON object describing how to proceed. Here are your possible choices:
-
-            1. If more information from a single product manual would be needed to answer the agent's question, reply
-              { "needMoreInfo": true, "searchProductId": number, "searchPhrase": string }.
-            2. If more information from ALL product manuals would be needed to answer the agent's question, reply
-              { "needMoreInfo": true, "searchPhrase": string }.
-            3. If the context already gives enough information to answer the agent's question, reply
-              { "needMoreInfo": false } but DO NOT ACTUALLY ANSWER THE QUESTION. Your response must NOT have any other information than this single boolean value.
-
-            If this is a question about the product, ALWAYS set needMoreInfo to true and search the product manual.
-            """);
-
-        var toolOutputs = new List<ChatMessageContent>();
-        for (var iteration = 0; iteration < 3; iteration++)
+        [KernelFunction]
+        public async Task<object> SearchManual(
+            [Description("A phrase to use when searching the manual")] string searchPhrase,
+            [Description("ID for the product whose manual to search. Set to null only if you must search across all product manuals.")] int? productId)
         {
-            var action = await GetNextAction(chatService, chatHistory, cancellationToken);
-            if (string.IsNullOrWhiteSpace(action?.SearchPhrase))
-            {
-                break;
-            }
+            await httpContext.Response.WriteAsync(",\n");
+            await httpContext.Response.WriteAsync(JsonSerializer.Serialize(new AssistantChatReplyItem(AssistantChatReplyItemType.Search, searchPhrase)));
 
-            await httpResponse.WriteAsync(",\n");
-            await httpResponse.WriteAsync(JsonSerializer.Serialize(new AssistantChatReplyItem(AssistantChatReplyItemType.Search, action.SearchPhrase )));
-
-            var searchResults = await manualSearch.SearchAsync(action.SearchProductId, action.SearchPhrase);
-            var toolOutputMessage = new ChatMessageContent(AuthorRole.System, $"""
-                The assistant performed a search with term "{action.SearchPhrase}" on the user manual,
-                which returned the following results:
-                {string.Join("\n", searchResults.Select(r => $"<search_result productId=\"{GetProductId(r)}\" searchResultId=\"{r.Metadata.Id}\">{r.Metadata.Text}</search_result>"))}
-
-                Based on this, decide again how to proceed using the same rules as before.
-                """);
-            chatHistory.Add(toolOutputMessage);
-            toolOutputs.Add(toolOutputMessage);
+            var searchResults = await manualSearch.SearchAsync(productId, searchPhrase);
 
             foreach (var r in searchResults)
             {
-                await httpResponse.WriteAsync(",\n");
-                await httpResponse.WriteAsync(JsonSerializer.Serialize(new AssistantChatReplyItem(
+                await httpContext.Response.WriteAsync(",\n");
+                await httpContext.Response.WriteAsync(JsonSerializer.Serialize(new AssistantChatReplyItem(
                     AssistantChatReplyItemType.SearchResult,
                     string.Empty,
                     int.Parse(r.Metadata.Id),
                     GetProductId(r),
                     GetPageNumber(r))));
             }
+
+            return searchResults.Select(r => new
+            {
+                ProductId = GetProductId(r),
+                SearchResultId = r.Metadata.Id,
+                r.Metadata.Text,
+            });
         }
-
-        return toolOutputs;
-    }
-
-    private static async Task<NextActionReply?> GetNextAction(IChatCompletionService chatService, ChatHistory chatHistory, CancellationToken cancellationToken)
-    {
-        var executionSettings = new OpenAIPromptExecutionSettings { ResponseFormat = "json_object", Seed = 0, Temperature = 0 };
-        var response = await chatService.GetChatMessageContentAsync(chatHistory, executionSettings, cancellationToken: cancellationToken);
-        chatHistory.Add(response);
-        return TryParseNextActionReply(response.ToString(), out var reply) ? reply : null;
     }
 
     private static int? GetProductId(MemoryQueryResult result)
@@ -170,25 +135,5 @@ public static class AssistantApi
     {
         var match = Regex.Match(result.Metadata.AdditionalMetadata, @"pagenumber:(\d+)");
         return match.Success ? int.Parse(match.Groups[1].Value) : null;
-    }
-
-    private static bool TryParseNextActionReply(string reply, [NotNullWhen(true)] out NextActionReply? assistantReply)
-    {
-        try
-        {
-            assistantReply = JsonSerializer.Deserialize<NextActionReply>(reply, _jsonOptions)!;
-            return true;
-        }
-        catch
-        {
-            assistantReply = null;
-            return false;
-        }
-    }
-
-    public class NextActionReply
-    {
-        public int? SearchProductId { get; set; }
-        public string? SearchPhrase { get; set; }
     }
 }
