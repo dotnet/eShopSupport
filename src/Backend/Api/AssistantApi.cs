94cc4466e62126204f4ff50e5b2dbdc2ce917218
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2024-07-19 13:28:48 +0100

    Add comments to assistant API code

--- src/Backend/Api/AssistantApi.cs
+++ src/Backend/Api/AssistantApi.cs
@@ -23,15 +23,12 @@ public static class AssistantApi
 
     private static async Task GetStreamingChatResponseAsync(AssistantChatRequest request, HttpContext httpContext, AppDbContext dbContext, IChatCompletionService chatService, ProductManualSemanticSearch manualSearch, ILoggerFactory loggerFactory, CancellationToken cancellationToken)
     {
-        await httpContext.Response.WriteAsync("[null");
-
         var product = request.ProductId.HasValue
             ? await dbContext.Products.FindAsync(request.ProductId.Value)
             : null;
 
-        var chatHistory = new ChatHistory();
-
-        chatHistory.AddSystemMessage($$"""
+        // Build the prompt plus any existing conversation history
+        var chatHistory = new ChatHistory($$"""
             You are a helpful AI assistant called 'Assistant' whose job is to help customer service agents working for AdventureWorks, an online retailer.
             The customer service agent is currently handling the following ticket:
 
@@ -53,21 +50,26 @@ public static class AssistantApi
             """);
 
         chatHistory.AddRange(request.Messages.Select(m => new ChatMessageContent(m.IsAssistant ? AuthorRole.Assistant : AuthorRole.User, m.Text)));
+        await httpContext.Response.WriteAsync("[null");
 
+        // Call the LLM backend
         var kernel = new Kernel();
         kernel.ImportPluginFromObject(new SearchManualPlugin(httpContext, manualSearch));
-
         var executionSettings = new OpenAIPromptExecutionSettings { Seed = 0, Temperature = 0, ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+        var streamingAnswer = chatService.GetStreamingChatMessageContentsAsync(chatHistory, executionSettings, kernel, cancellationToken);
+
+        // Stream the response to the UI
         var answerBuilder = new StringBuilder();
-        await foreach (var chunk in chatService.GetStreamingChatMessageContentsAsync(chatHistory, executionSettings, kernel, cancellationToken: cancellationToken))
+        await foreach (var chunk in streamingAnswer)
         {
             await httpContext.Response.WriteAsync(",\n");
             await httpContext.Response.WriteAsync(JsonSerializer.Serialize(new AssistantChatReplyItem(AssistantChatReplyItemType.AnswerChunk, chunk.ToString())));
             answerBuilder.Append(chunk.ToString());
         }
 
+        // Ask if this answer is suitable for sending directly to the customer
+        // If so, we'll show a button in the UI
         chatHistory.AddAssistantMessage(answerBuilder.ToString());
-
         chatHistory.AddSystemMessage("""
             Consider the answer you just gave and decide whether it is addressed to the customer by name as a reply to them.
             Reply as a JSON object in this form: { "isAddressedByNameToCustomer": trueOrFalse }.
@@ -85,6 +87,7 @@ public static class AssistantApi
         }
         catch { }
 
+        // Signal to the UI that we're finished
         await httpContext.Response.WriteAsync("]");
     }
 
@@ -100,11 +103,12 @@ public static class AssistantApi
             [Description("A phrase to use when searching the manual")] string searchPhrase,
             [Description("ID for the product whose manual to search. Set to null only if you must search across all product manuals.")] int? productId)
         {
+            // Notify the UI we're doing a search
             await httpContext.Response.WriteAsync(",\n");
             await httpContext.Response.WriteAsync(JsonSerializer.Serialize(new AssistantChatReplyItem(AssistantChatReplyItemType.Search, searchPhrase)));
 
+            // Do the search, and supply the results to the UI so it can show one as a citation link
             var searchResults = await manualSearch.SearchAsync(productId, searchPhrase);
-
             foreach (var r in searchResults)
             {
                 await httpContext.Response.WriteAsync(",\n");
@@ -116,6 +120,7 @@ public static class AssistantApi
                     GetPageNumber(r))));
             }
 
+            // Return the search results to the assistant
             return searchResults.Select(r => new
             {
                 ProductId = GetProductId(r),
