79e3be100c48b1b1022d77a4268b543fa6e0aa5f
Damian Edwards <damian@damianedwards.com>
2024-10-11 16:50:47 -0700

    Tweaks

--- src/ServiceDefaults/Extensions.cs
+++ src/ServiceDefaults/Extensions.cs
@@ -49,7 +49,8 @@ public static class Extensions
             })
             .WithTracing(tracing =>
             {
-                tracing.AddAspNetCoreInstrumentation()
+                tracing.AddSource(builder.Environment.ApplicationName)
+                    .AddAspNetCoreInstrumentation()
                     // Uncomment the following line to enable gRPC instrumentation (requires the OpenTelemetry.Instrumentation.GrpcNetClient package)
                     //.AddGrpcClientInstrumentation()
                     .AddHttpClientInstrumentation()
