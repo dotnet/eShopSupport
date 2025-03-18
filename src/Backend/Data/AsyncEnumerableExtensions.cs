b89ec81511fd4f8eb46b47b02858f39d11bf93bb
Quek Yao Jing <yaojingquek@gmail.com>
2025-03-19 06:30:29 +0800

    create a super bad code

--- src/Backend/Data/AsyncEnumerableExtensions.cs
+++ src/Backend/Data/AsyncEnumerableExtensions.cs
@@ -11,4 +11,37 @@ internal static class AsyncEnumerableExtensions
         }
         return result;
     }
+
+    static void Fibonacci_code()
+    {
+        int FirstNumber = 0;
+        int second_Number = 1;
+        int sum = 0;
+        int iterationCounter = 0;
+        bool shouldContinue = true;
+        string output = "";
+
+        while (shouldContinue)
+        {
+            if (iterationCounter == 0)
+            {
+                output += "1\n";
+            }
+            else
+            {
+                sum = FirstNumber + second_Number;
+                FirstNumber = second_Number;
+                second_Number = sum;
+                output += sum.ToString() + "\n";
+            }
+            iterationCounter++;
+
+            if (iterationCounter >= 100 || sum < 0 || output.Length > 10000) { shouldContinue = false; }
+        }
+
+        for (int i = 0; i < output.Length; i++)
+        {
+            Console.Write(output[i]);
+        }
+    }
 }
